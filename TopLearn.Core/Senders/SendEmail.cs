e980f47ecb5356f39fcc17480e31c48ebd22bf69
armin habibi <workwitharmin7@gmail.com>
2020-07-29 00:55:30 +0430

    Email

--- TopLearn.Core/Senders/SendEmail.cs
+++ TopLearn.Core/Senders/SendEmail.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Mail;
+using System.Net;
 using System.Web;
 
 namespace TopLearn.Core.Senders
@@ -11,20 +12,19 @@ namespace TopLearn.Core.Senders
         public static void Sendemail(string to,string subject,string body)
         {
             MailMessage mail = new MailMessage();
-            SmtpClient SmtpServer = new SmtpClient("smtp.gmail.com");
-            mail.From = new MailAddress("arminprgrmcsfamily@gmail.com","تاپ لرن");
+            mail.From = new MailAddress("workatarmin@gmail.com","تاپ لرن");
             mail.To.Add(to);
             mail.Subject = subject;
             mail.Body = body;
             mail.IsBodyHtml = true;
+            SmtpClient SmtpServer = new SmtpClient("smtp.gmail.com");
 
             //Attachment attachment = new Attachment("D:/textfile.txt");
             //mail.Attachments.Add(attachment);
 
             SmtpServer.Port = 587;
-            SmtpServer.Credentials = new System.Net.NetworkCredential("arminprgrmcsfamily@gmail.com","armin@_prgrm_@cs82");
-            SmtpServer.EnableSsl = false;
-
+            SmtpServer.Credentials = new NetworkCredential("mytestformycode@gmail.com","hiS&29Do25c#");
+            SmtpServer.EnableSsl = true;
             SmtpServer.Send(mail);
 
         }
