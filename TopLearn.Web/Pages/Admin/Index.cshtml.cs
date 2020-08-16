efb8d5042f34a3713a8c98f6f1efeb0916bd5fc1
armin habibi <workwitharmin7@gmail.com>
2020-08-16 13:23:08 +0430

    Admin

--- TopLearn.Web/Pages/Admin/Index.cshtml.cs
+++ TopLearn.Web/Pages/Admin/Index.cshtml.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace TopLearn.Web.Pages
+namespace TopLearn.Web.Pages.Admin
 {
     public class IndexModel : PageModel
     {
